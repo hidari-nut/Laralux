fe1d52829f7a53e2b28e97b97bf31c256bf814f1
hidari-nut <madevis02@gmail.com>
2024-06-26 14:45:40 +0700

    Update profile.blade.php

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -3,60 +3,79 @@
     <!-- Profile Edit Start -->
 
     <div class="container">
-        <div class="row justify-content-left">
+        <div class="row justify-content-center">
             <div class="col-lg-8">
-
-                <h4 class="mb-4 text-right">Edit <span class="text-primary">Profile</span></h4>
-                <form>
-                    <div class="row g-3">
-                        <div class="col-md-6">
-                            <div class="form-floating">
-                                <input type="text" class="form-control" id="username" placeholder="Username">
-                                <label for="username">Username</label>
+                <div class="bg-white shadow p-4 rounded">
+                    <h4 class="mb-4 text-center">Edit <span class="text-primary">Profile</span></h4>
+                    <form>
+                        <div class="row g-3">
+                            <div class="col-md-12 text-center">
+                                <img src="https://picsum.photos/200" class="rounded-circle" alt="Profile Picture">
                             </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-floating">
-                                <input type="email" class="form-control" id="email" placeholder="Email">
-                                <label for="email">Email</label>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <label for="password" class="form-label">Password</label>
-                            <div class="input-group">
-                                <input type="password" id="password" name="password"
-                                    class="form-control @error('password') is-invalid @enderror" required
-                                    autocomplete="new-password">
-                                <div class="input-group-append">
-                                    <button type="button" class="btn btn-outline-secondary" id="togglePassword">
-                                        <i class="fa fa-eye"></i>
-                                    </button>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="firstName" placeholder="First Name">
+                                    <label for="firstName">First Name</label>
                                 </div>
                             </div>
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                        <div class="col-md-6">
-                            <label for="password-confirm" class="form-label">Re-enter Password</label>
-                            <div class="input-group">
-                                <input type="password" id="password-confirm" name="password_confirmation"
-                                    class="form-control" required autocomplete="new-password">
-                                <div class="input-group-append">
-                                    <button type="button" class="btn btn-outline-secondary" id="togglePasswordConfirm">
-                                        <i class="fa fa-eye"></i>
-                                    </button>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="lastName" placeholder="Last Name">
+                                    <label for="lastName">Last Name</label>
                                 </div>
                             </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="email" class="form-control" id="email" placeholder="Email">
+                                    <label for="email">Email</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="username" placeholder="Username">
+                                    <label for="username">Username</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <label for="password" class="form-label">Password</label>
+                                <div class="input-group">
+                                    <input type="password" id="password" name="password"
+                                        class="form-control @error('password') is-invalid @enderror" required
+                                        autocomplete="new-password">
+                                    <div class="input-group-append">
+                                        <button type="button" class="btn btn-outline-secondary" id="togglePassword">
+                                            <i class="fa fa-eye"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6">
+                                <label for="password-confirm" class="form-label">Re-enter Password</label>
+                                <div class="input-group">
+                                    <input type="password" id="password-confirm" name="password_confirmation"
+                                        class="form-control" required autocomplete="new-password">
+                                    <div class="input-group-append">
+                                        <button type="button" class="btn btn-outline-secondary" id="togglePasswordConfirm">
+                                            <i class="fa fa-eye"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <label for="profile_image" class="form-label">Profile Picture</label>
+                                <input type="file" id="profile_image" name="profile_image" class="form-control">
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary w-100 py-3" type="submit">Save Changes</button>
+                            </div>
                         </div>
-                        <div class="col-12">
-                            <button class="btn btn-primary w-100 py-3" type="submit">Save Changes</button>
-                        </div>
-                    </div>
-                </form>
-
+                    </form>
+                </div>
             </div>
         </div>
     </div>
