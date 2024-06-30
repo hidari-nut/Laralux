060272db0fed4e2556f40531a768f086d54894ae
Pramodia Suka <71862264+pramodiaSuka@users.noreply.github.com>
2024-06-30 15:26:30 +0700

    Authorization Debug Adding profile route when login as owner

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,8 +13,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // User::create(
+        //     ["name"=>"Aston Inn Staff", "email"=>"astoninn@staff.com", "email_verified_at"=>now(), "password" => bcrypt("astoninn"), "img"=>"1719407741_fotoku.jpg", "created_at"=>now(), "updated_at"=>now(), "roles_id"=>2]
+        // );
         User::create(
-            ["name"=>"Aston Inn Staff", "email"=>"astoninn@staff.com", "email_verified_at"=>now(), "password" => bcrypt("astoninn"), "img"=>"1719407741_fotoku.jpg", "created_at"=>now(), "updated_at"=>now(), "roles_id"=>2]
+            ["name"=>"Aston Inn Owner", "email"=>"astoninn@owner.com", "email_verified_at"=>now(), "password" => bcrypt("astoninnowner"), "img"=>"noimage.jpeg", "created_at"=>now(), "updated_at"=>now(), "roles_id"=>1]
         );
     }
 }
